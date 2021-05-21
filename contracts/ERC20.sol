4514803674bdcde3a0c43c17c6314a83dea4603d
solenemhep <solene.pettier@gmail.com>
2021-05-21 14:04:47 +0200

    contract

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -5,12 +5,14 @@ import "hardhat/console.sol";
 
 contract ERC20 {
     mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
     string private _name;
     string private _symbol;
 
-    event Transfer(
-        address indexed sender,
-        address indexed recipient,
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
         uint256 amount
     );
 
@@ -19,6 +21,47 @@ contract ERC20 {
         _symbol = "CC";
         _balances[owner_] = initialSupply_;
         emit Transfer(address(0), owner_, initialSupply_);
+    }
+
+    // functions
+    function transfer(address to, uint256 amount) public {
+        require(
+            _balances[msg.sender] >= amount,
+            "SmartWallet: Not enough Ether to transfer"
+        );
+        require(to != address(0), "SmartWallet: transfer to the zero address");
+        _balances[msg.sender] -= amount;
+        _balances[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public {
+        require(
+            _allowances[from][msg.sender] >= amount,
+            "SmartWallet: transfer amount exceeds allowance"
+        );
+        require(
+            _balances[from] >= amount,
+            "SmartWallet: Not enough Ether to transfer"
+        );
+        require(to != address(0), "SmartWallet: transfer to the zero address");
+        _allowances[from][msg.sender] -= amount;
+        _balances[from] -= amount;
+        _balances[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function approve(address spender, uint256 amount) public {
+        require(
+            spender != address(0),
+            "SmartWallet: approve to the zero address"
+        );
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
     }
 
     // getters
@@ -32,5 +75,13 @@ contract ERC20 {
 
     function balanceOf(address account) public view returns (uint256) {
         return _balances[account];
+    }
+
+    function allowance(address owner, address spender)
+        public
+        view
+        returns (uint256)
+    {
+        return _allowances[owner][spender];
     }
 }
--- test/ERC20-test.js
+++ test/ERC20-test.js
@@ -30,13 +30,39 @@ describe("ERC20", function () {
   });
 
   describe("Transactions", function () {
-    it("Emits event Transfer when transfer token", async function () { })
-    it("Emits event Transfer when transferFrom token", async function () { })
 
-  });
+    describe("transfer", function () {
+      it('Should transfer tokens', async () => { })
+      it('Should fail if msg.sender doesnt have enough tokens', async () => {
+        await expect().to.be.revertedWith("ERC20: Not enough Ether to transfer");
+      });
+      it('Should fail if msg.sender is address(0)', async () => {
+        await expect().to.be.revertedWith("ERC20: transfer to the zero address");
+      });
+      it("Emits event Transfer when transfer token", async function () { })
+    });
 
-  describe("", function () {
+    describe("transferFrom", function () {
+      it('Should transferFrom tokens', async () => { });
+      it('Should fail if msg.sender is not allowed by from', async () => {
+        await expect().to.be.revertedWith("ERC20: transfer amount exceeds allowance");
+      });
+      it('Should fail if from doesnt have enough tokens', async () => {
+        await expect().to.be.revertedWith("ERC20: Not enough Ether to transfer");
+      });
+      it('Should fail if from is address(0)', async () => {
+        await expect().to.be.revertedWith("ERC20: transfer to the zero address");
+      });
+      it("Emits event Transfer when transferFrom token", async function () { })
+    });
 
-  });
+    describe("approve", function () {
+      it('Should approve allowances', async () => { });
+      it('Should fail if spender is address(0)', async () => {
+        await expect().to.be.revertedWith("ERC20: approve to the zero address");
+      });
+      it("Emits event Approval when approve spender", async function () { })
+    });
 
-});
+  })
+})
